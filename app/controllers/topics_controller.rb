339ac233bb6f9cb54fb76b9ee342456ae9ba68dd
名前 <>
2019-12-30 05:31:26 +0000

    あげなおし　Rails 06 写真投稿機能の作成

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,7 @@
 class TopicsController < ApplicationController
-  def index
-    @topics = Topic.all.includes(:favorite_users)
-   end 
+def index
+  @topics = Topic.all.includes(:favorite_users)
+end
   
   def new
     @topic = Topic.new
